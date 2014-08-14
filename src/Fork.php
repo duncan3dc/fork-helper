ad8891085214965f61d7c0ba67e986c1ba840a2a
Craig Duncan <git@duncanc.co.uk>
2014-08-14 09:20:55 +0100

    Catch exceptions and display them all at the end
    Previously all we had access to was the return code of the threads, but by using shared memory we can store some of the exception details to output at the end

--- src/Fork.php
+++ src/Fork.php
@@ -4,7 +4,9 @@ namespace duncan3dc\Helpers;
 
 class Fork
 {
+    const SHARED_MEMORY_LIMIT = 1000;
     private $threads;
+    private $memoryKey;
     public  $ignoreErrors;
 
 
@@ -12,6 +14,7 @@ class Fork
     {
         $this->threads = [];
         $this->ignoreErrors = false;
+        $this->memoryKey = round(microtime(true) * 1000);
     }
 
 
@@ -19,13 +22,26 @@ class Fork
     {
         $pid = pcntl_fork();
 
-        if($pid == -1) {
+        if ($pid == -1) {
             throw new \Exception("Failed to fork");
         }
 
         # If this is the child process, then run the requested function
-        if(!$pid) {
-            $func();
+        if (!$pid) {
+            try {
+                $func();
+            } catch(\Exception $e) {
+                $memory = shmop_open($this->memoryKey, "c", 0644, static::SHARED_MEMORY_LIMIT);
+                $errors = shmop_read($memory, 0, static::SHARED_MEMORY_LIMIT);
+                $errors = trim($errors);
+                if ($errors) {
+                    $errors .= "\n";
+                }
+                $errors .= "Exception: " . $e->getMessage() . " (" . $e->getFile() . ":" . $e->getLine() . ")";
+                shmop_write($memory, $errors, 0);
+                shmop_close($memory);
+                exit(1);
+            }
 
             # Then we must exit or else we will end up the child process running the parent processes code
             die();
@@ -39,23 +55,32 @@ class Fork
 
     public function wait($pid = false)
     {
-        if($pid) {
+        if ($pid) {
             $threads = [$pid];
         } else {
             $threads = $this->threads;
         }
 
         $error = false;
-        foreach($threads as $pid) {
+        foreach ($threads as $pid) {
             pcntl_waitpid($pid, $status);
-            if($status > 0) {
+            if ($status > 0) {
                 $error = $status;
             }
             unset($this->threads[$pid]);
         }
 
-        if(!$this->ignoreErrors && $error) {
-            throw new \Exception("An error occurred within a thread, the return code was (" . $error . ")");
+        if (!$this->ignoreErrors && $error) {
+            $memory = shmop_open($this->memoryKey, "a", 0, 0);
+            $errors = shmop_read($memory, 0, static::SHARED_MEMORY_LIMIT);
+            shmop_delete($memory);
+            shmop_close($memory);
+
+            $error = "An error occurred within a thread, the return code was (" . $error . ")";
+            if ($errors = trim($errors)) {
+                $error .= "\n" . $errors;
+            }
+            throw new \Exception($error);
         }
 
         return $status;
