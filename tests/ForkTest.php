b95bf5e72d3fba475a0f4407373004862946fe91
Craig Duncan <git@duncanc.co.uk>
2014-09-20 19:57:47 +0100

    Added a simple test case

---
+++ tests/ForkTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace duncan3dc\Helpers;
+
+class ForkTest extends \PHPUnit_Framework_TestCase
+{
+    public function testMain()
+    {
+        $output = "";
+        $memoryKey = round(microtime(true) * 1000);
+        $memoryLimit = 100;
+
+        $writeToMemory = function($string) use(&$output, $memoryKey, $memoryLimit) {
+            $memory = shmop_open($memoryKey, "c", 0644, $memoryLimit);
+            $output = shmop_read($memory, 0, $memoryLimit);
+            if ($output = trim($output)) {
+                $output .= "\n";
+            }
+            $output .= $string;
+            shmop_write($memory, $output, 0);
+            shmop_close($memory);
+        };
+
+        $fork = new Fork;
+
+        $fork->call(function() use($writeToMemory) {
+            $writeToMemory("func1.1");
+            usleep(80000);
+            $writeToMemory("func1.2");
+        });
+
+        $fork->call(function() use ($writeToMemory) {
+            usleep(50000);
+            $writeToMemory("func2.1");
+            usleep(50000);
+            $writeToMemory("func2.2");
+        });
+
+        usleep(75000);
+        $writeToMemory("waiting");
+
+        $fork->wait();
+        $writeToMemory("end");
+
+        $memory = shmop_open($memoryKey, "a", 0, 0);
+        $output = trim(shmop_read($memory, 0, $memoryLimit));
+        shmop_delete($memory);
+        shmop_close($memory);
+
+        $this->assertSame("func1.1\nfunc2.1\nwaiting\nfunc1.2\nfunc2.2\nend", $output);
+    }
+}
