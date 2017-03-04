2a62d6089dbad316bdd71438fdffde03ae11f439
Craig Duncan <git@duncanc.co.uk>
2017-03-04 19:17:59 +0000

    Allow memory to be deleted when we're finished

--- tests/SharedMemoryTest.php
+++ tests/SharedMemoryTest.php
@@ -4,6 +4,7 @@ namespace duncan3dc\ForkerTests;
 
 use duncan3dc\Forker\SharedMemory;
 use duncan3dc\ObjectIntruder\Intruder;
+use PHPUnit\Framework\Error\Error;
 use PHPUnit\Framework\TestCase;
 
 class SharedMemoryTest extends TestCase
@@ -14,7 +15,17 @@ class SharedMemoryTest extends TestCase
     {
         error_reporting(\E_ALL);
 
-        $this->memory = new SharedMemory;
+        $memory = new SharedMemory;
+        $this->memory = new Intruder($memory);
+    }
+
+
+    public function tearDown()
+    {
+        try {
+            $this->memory->delete();
+        } catch (\Throwable $e) {
+        }
     }
 
 
@@ -31,6 +42,9 @@ class SharedMemoryTest extends TestCase
         $key2 = (new Intruder($memory2))->key;
 
         $this->assertGreaterThan($key1, $key2);
+
+        $memory1->delete();
+        $memory2->delete();
     }
 
 
@@ -80,5 +94,18 @@ class SharedMemoryTest extends TestCase
         $exceptions = $this->memory->getExceptions();
         $this->assertSame(1, count($exceptions));
         $this->assertStringMatchesFormat("DomainException: Nope (%s:%s)", $exceptions[0]);
+    }
+
+
+    public function testDelete()
+    {
+        # Get the shared memory key that the instance is using
+        $key = $this->memory->key;
+
+        $this->memory->delete();
+
+        # Make sure the memory has been cleaned up by attempting to access it
+        $this->expectException(Error::class, "shmop_open(): unable to attach or create shared memory segment 'No such file or directory'");
+        shmop_open($key, "a", 0, 0);
     }
 }
